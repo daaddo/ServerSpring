7436e03aaf0504a1e90e8a0707330b59f0a052c3
DaviDio <david.cascella.5@gmail.com>
2024-07-11 18:11:00 +0200

    created type enum and added values to task and user

--- src/main/java/it/cascella/redisBridge/entities/Task.java
+++ src/main/java/it/cascella/redisBridge/entities/Task.java
@@ -1,4 +1,20 @@
 package it.cascella.redisBridge.entities;
 
+import jakarta.persistence.*;
+
 public class Task {
+    @Id
+    private Long id;
+
+    private String text;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }
--- src/main/java/it/cascella/redisBridge/entities/User.java
+++ src/main/java/it/cascella/redisBridge/entities/User.java
@@ -1,4 +1,24 @@
 package it.cascella.redisBridge.entities;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+
 }
